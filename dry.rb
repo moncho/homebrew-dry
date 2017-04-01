bd9b66de95f06279976116f61e352c4529ebbe1a
moncho <ramon.cr@gmail.com>
2017-04-01 03:38:49 +0200

    Update dry version

--- dry.rb
+++ dry.rb
@@ -1,11 +1,11 @@
-DRY_VERSION= "0.7-beta.2".freeze
+DRY_VERSION= "0.7-beta.4".freeze
 
 class Dry < Formula
   desc "docker container manager for the terminal"
   homepage "https://moncho.github.io/dry/"
-  url "https://github.com/moncho/dry/releases/download/v0.7-beta.2/dry-darwin-amd64"
-  version "v0.7-beta.2"
-  sha256 "db37452126cc6f222e29db4b7dfb3e3e9a66c75a3e9b7e841b6e19469ce7ae75"
+  url "https://github.com/moncho/dry/releases/download/v0.7-beta.4/dry-darwin-amd64"
+  version "v0.7-beta.4"
+  sha256 "82f820b2812a11a5f2732118617727ea599303801be6b1e4037f224307089447"
 
   def install
     mv "dry-darwin-amd64", "dry"
