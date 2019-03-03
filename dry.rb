f087c1b045db322eb272901cba4f2374bfb07748
moncho <50428+moncho@users.noreply.github.com>
2019-03-03 17:43:37 +0100

    Update version

--- dry.rb
+++ dry.rb
@@ -1,14 +1,14 @@
-DRY_VERSION= "v0.9-beta.8".freeze
+DRY_VERSION= "v0.9-beta.9".freeze
 
 class Dry < Formula
   desc "a Docker manager for the terminal"
   homepage "https://moncho.github.io/dry/"
-  version "v0.9-beta.8"
+  version "v0.9-beta.9"
   if OS.mac?
-    sha256 "24176c235a49abcd4f67ea1dec49a60ccb3c1596339f1718a91133e5303dff9b"
+    sha256 "013aeb0123632605f03374f3ffbb708d90b9f0881719ffd231b3ed2e2358e204"
     url "https://github.com/moncho/dry/releases/download/#{version}/dry-darwin-amd64"
   elsif OS.linux?
-    sha256 "17ae95eca8f2f16de873b1506c311241a4b9359e035e00f201828ef84c8e4931"
+    sha256 "556b680f2e7680b91901b6562670f616917cda0a5fb1b0c72c39e5fa63852dad"
     url "https://github.com/moncho/dry/releases/download/#{version}/dry-linux-amd64"
   end
 
