4010cfb7b71a5c93871eab0b714637c9694a9bd2
moncho <50428+moncho@users.noreply.github.com>
2019-08-16 13:19:08 +0200

    Bump dry to version v0.10-beta.1

--- dry.rb
+++ dry.rb
@@ -1,14 +1,14 @@
-DRY_VERSION= "v0.9-beta.10".freeze
+DRY_VERSION= "v0.10-beta.1".freeze
 
 class Dry < Formula
   desc "a Docker manager for the terminal"
   homepage "https://moncho.github.io/dry/"
-  version "v0.9-beta.10"
+  version "v0.10-beta.1"
   if OS.mac?
-    sha256 "5144f27deeb9da8cd8fa93a59caf8b200faf8a9e31b0e3e03ff4efb187305ebe"
+    sha256 "94e499a3a2e085e2b2fcc3dc2522a820161fb55b01b565ee51b88c21681b18c3"
     url "https://github.com/moncho/dry/releases/download/#{version}/dry-darwin-amd64"
   elsif OS.linux?
-    sha256 "a7467648886fb898491f2cafb868c4f928c33a1a252f7e70bfe79986a8f074fb"
+    sha256 "e2865e54d5ff825b2a2b5f52b8271d86fd51eff868dd8a7cf3a7b81fa854a660"
     url "https://github.com/moncho/dry/releases/download/#{version}/dry-linux-amd64"
   end
 
